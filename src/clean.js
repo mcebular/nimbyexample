00c2edacdf1acf09ff122785357082a774a6db07
mcebular <martin.cebular@gmail.com>
2022-03-08 21:44:36 +0100

    use rmSync instead of rmdirSync (deprecation warning)

--- src/clean.js
+++ src/clean.js
@@ -5,7 +5,7 @@ const cleanBuild = () => {
     // remove all generated file in build path
     if (fs.existsSync(BUILD_PATH)) {
         console.log("Removing " + BUILD_PATH + " folder.");
-        fs.rmdirSync(BUILD_PATH, { recursive: true });
+        fs.rmSync(BUILD_PATH, { recursive: true });
     }
 }
 
